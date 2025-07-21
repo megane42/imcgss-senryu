43eab95e828118b9796612223fa99ddd1fb1b6b9
megane42 <h.kzm.gm@gmail.com>
2025-07-22 01:01:18 +0900

    add reset css

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+    const content: string
+    export default content
+}
+
+declare module 'modern-normalize' {
+    const content: string
+    export default content
+}
