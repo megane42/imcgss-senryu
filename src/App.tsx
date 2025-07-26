ca0b1a16421d7b3a5ec8ab817d886e6a3a7eaf0f
megane42 <h.kzm.gm@gmail.com>
2025-07-27 04:33:33 +0900

    separate tweet button from SenryuCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { Logo } from '@/components/Logo'
 import { SenryuCard } from '@/components/SenryuCard'
+import { TweetButton } from '@/components/TweetButton'
+import { useSenryu } from '@/hooks/useSenryu'
 import styles from './App.module.css'
 
 function App() {
+  const { senryu, error, handleGenerateSenryu } = useSenryu()
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <Logo />
       </header>
       <main className={styles.main}>
-        <SenryuCard />
+        <div className={styles.senryuCardContainer}>
+          <SenryuCard 
+            senryu={senryu} 
+            error={error} 
+            onPush={handleGenerateSenryu} 
+          />
+        </div>
+        <div className={styles.tweetButtonContainer}>
+          {senryu && <TweetButton senryu={senryu} />}
+        </div>
       </main>
     </div>
   )
