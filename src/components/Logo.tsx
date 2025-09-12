be2dc708c2cdd1cba72ecb4c1d3c3dc93218b445
megane42 <h.kzm.gm@gmail.com>
2025-09-13 04:47:28 +0900

    remove hard-coded base path

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css'
 export const Logo: React.FC = () => {
   return (
     <h1 className={styles.logo}>
-      <a href="/imcgss-senryu/" className={styles.logoLink}>
+      <a href={import.meta.env.BASE_URL} className={styles.logoLink}>
         <span className={styles.logoFormar}>デレマス</span>
         <span className={styles.logoLatter}>川柳</span>
       </a>
