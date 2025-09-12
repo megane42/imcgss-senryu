029d82a28ddfcc7bed70f2610a7b96867fe3457a
megane42 <h.kzm.gm@gmail.com>
2025-09-13 03:35:48 +0900

    make logo clickable

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,10 @@ import styles from './Logo.module.css'
 export const Logo: React.FC = () => {
   return (
     <h1 className={styles.logo}>
-      <span className={styles.logoFormar}>デレマス</span>
-      <span className={styles.logoLatter}>川柳</span>
+      <a href="/imcgss-senryu/" className={styles.logoLink}>
+        <span className={styles.logoFormar}>デレマス</span>
+        <span className={styles.logoLatter}>川柳</span>
+      </a>
     </h1>
   )
 }
