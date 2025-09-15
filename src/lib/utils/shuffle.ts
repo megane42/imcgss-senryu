3aada723df6387799541f1539cfaa1efb5617e7e
megane42 <h.kzm.gm@gmail.com>
2025-09-15 22:52:35 +0900

    fix randomeness (or just my imagination?)

--- src/lib/utils/shuffle.ts
+++ src/lib/utils/shuffle.ts
@@ -3,7 +3,10 @@ export const shuffle = <T>(array: T[]): T[] => {
 
   // Fisher-Yates shuffle algorithm
   for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    // Use crypto.getRandomValues for better randomness
+    const randomArray = new Uint32Array(1);
+    crypto.getRandomValues(randomArray);
+    const j = randomArray[0] % (i + 1);
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
 
